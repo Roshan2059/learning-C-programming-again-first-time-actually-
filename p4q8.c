d0b162afa643dfc35ef65db3faa1ada841d651a6
Roshan <roshanpanta2059@gmail.com>
2022-04-10 16:23:54 +0545

    done upto p4q8

--- p4q8.c
+++ p4q8.c
@@ -1,1 +1,29 @@
-djsahjdsa
+#include<stdio.h>
+
+int main(){
+	int i, n, mul;
+	
+	printf("Enter the number to find the factorial of\n");
+	scanf("%d", &n);
+	
+	for(i = 1; i <= n ; i++){
+		mul = mul * i;
+	}
+	
+	printf("The factorial is %d", mul);
+}
+//#include<stdio.h>
+//
+//int main(){
+//	int i, n, fact;
+//	
+//	printf("Enter the number to find the factorial of\n");
+//	scanf("%d", &n);
+//	
+//	for(i = 1; i <= n ; i++){
+//		fact = fact * i;
+//	}
+//	
+//	printf("The factorial is %d", fact);
+//	return 0;
+//}
