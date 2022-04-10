4d5ec55a122f056fbadd2b04071eeeebea198747
Roshan <roshanpanta2059@gmail.com>
2022-04-10 19:23:53 +0545

    now will do guess the number game after this

--- p4q10.c
+++ p4q10.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
-
+// a number that is divisible by itself and one.
 int main(){
-	int n, i;
-	
+	int n, i, prime = 1;
 	printf("Enter the number\n");
 	scanf("%d", &n);
-	
-	for(i = 0; i < n; i++){
-		if()
+	for(i = 2; i < n; i++){
+		if(n % i == 0){
+			prime = 0;
+			break;
+		}
+	}
+	if(prime == 0){	
+		printf("It is not a prime number.");
+	}
+	else{
+		printf("It is a prime number.");
 	}
 }
